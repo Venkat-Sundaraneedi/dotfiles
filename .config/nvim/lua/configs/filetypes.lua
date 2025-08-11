ca38d5cdbd6987101a18bab043608f83cc92e1b6
Venkat-Sundaraneedi <venkat.subrahmanyam.34@gmail.com>
2025-08-11 07:18:08 +0530

    refactoring the dot files

--- .config/nvim/lua/configs/filetypes.lua
+++
@@ -1,9 +0,0 @@
--- configs/filetypes.lua
-vim.filetype.add({
-	extension = {
-		sw = "sway",
-	},
-	filename = {
-		["Forc.toml"] = "toml", -- For Fuel project config files
-	},
-})
--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
--- plugins/init.lua
-return {
-	-- Import by category
-	{ import = "plugins.editor" },
-	{ import = "plugins.ui" },
-	{ import = "plugins.coding" },
-	{ import = "plugins.tools" },
-}
