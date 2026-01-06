ac5fc0196b007d8e4fcef5582ad2030ac1907923
Venkat-Sundaraneedi <venkat.subrahmanyam.34@gmail.com>
2026-01-06 11:25:42 +0530

    Home-Manager rebuild on 2026-01-06 11:25:31 - 2026-01-06 11:07 id id

--- mysystem/nixos/home-manager/es-de/flake.nix
+++ mysystem/nixos/home-manager/es-de/flake.nix
@@ -1,12 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use unstable for latest packages; change to nixos-24.11 if preferred
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-
   outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux"; # Matches your x86_64 setup
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    pname = "es-de";
+    version = "3.4.0";
+    src = pkgs.fetchurl {
+      url = "https://gitlab.com/es-de/emulationstation-de/-/package_files/246875981/download";
+      hash = "sha256-TLZs/JIwmXEc+g7d2D22R0SmKU4C4//Rnuhn93qI7H4=";
+    };
+    appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
   in {
-    packages.${system}.default = import ./ES.nix {inherit pkgs;}; # Calls your derivation
+    packages.${system}.default = pkgs.appimageTools.wrapType2 {
+      inherit pname version src;
+      extraInstallCommands = ''
+        install -m 444 -D ${appimageContents}/ES-DE.desktop -t $out/share/applications || true
+        substituteInPlace $out/share/applications/ES-DE.desktop \
+          --replace 'Exec=AppRun' 'Exec=${pname}' || true
+        cp -r ${appimageContents}/usr/share/icons $out/share || true
+      '';
+      extraPkgs = pkgs:
+        with pkgs; [
+          # Test first; add qt6.qtbase, qt6.qtsvg, libglvnd, xorg.libX11 if needed
+        ];
+      extraBwrapArgs = [
+        "--bind-try /home/greed/.config/retro/ROMs /home/greed/.config/retro/ROMs"
+      ];
+      dieWithParent = false;
+      meta = {
+        description = "EmulationStation Desktop Edition - A frontend for browsing and launching games from your multi-platform game collection";
+        homepage = "https://es-de.org";
+        platforms = [pkgs.stdenv.hostPlatform.system]; # Uses pkgs.stdenv (equivalent to your stdenv)
+        license = pkgs.lib.licenses.mit;
+      };
+    };
   };
 }
