d5bff1e6e1390fbe089b4c520435258ea6fd96d5
Venkat-Sundaraneedi <venkat.subrahmanyam.34@gmail.com>
2026-02-07 07:44:33 +0530

    feat: update jj config and fish config

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -107,7 +107,7 @@ function toggle_vi_mode
     end
 end
 
-fish_vi_key_bindings
+set -g fish_key_bindings fish_vi_key_bindings
 
 bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 bind -M insert alt-l forward-char
